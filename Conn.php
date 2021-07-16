48643615b687964c32e29b92ea21809e904a793f
Olatunji Olayemi <olayemi289@gmail.com>
2021-07-16 10:54:22 +0100

    sit

--- Conn.php
+++ Conn.php
@@ -1,11 +1,11 @@
 <?php
 $server='localhost';
-$user='root';
-$pass='';
-$database='lilovat';
-// $user='lilovat5';
-// $pass=':m9Jx+pLSN46f9';
-// $database='lilovat5_lilovat';
+// $user='root';
+// $pass='';
+// $database='lilovat';
+$user='lilovat5';
+$pass=':m9Jx+pLSN46f9';
+$database='lilovat5_lilovat';
 $conn = new mysqli($server, $user, $pass, $database);
 date_default_timezone_set('Africa/Lagos');
 // Check connection
